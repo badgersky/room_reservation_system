c74cc8fa457c3cb64f7779a5ef8b148c2be55079
badgersky <szymonek.ptasznik@gmail.com>
2023-03-07 19:11:16 +0100

    created view for modifying room info

--- service_app/urls.py
+++ service_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from service_app.views import add_room, show_all_rooms, delete_room
+from service_app.views import add_room, show_all_rooms, delete_room, modify_room
 
 
 urlpatterns = [
     path('room/new/', add_room),
     path('rooms/', show_all_rooms, name='room_list'),
     path('room/delete/<room_id>/', delete_room),
+    path('room/modify/<room_id>/', modify_room)
 ]
--- service_app/views.py
+++ service_app/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.utils.datastructures import MultiValueDictKeyError
-
 from service_app.models import Room
 
 
@@ -21,12 +20,8 @@ def add_room(request):
             if Room.objects.filter(name=room_name).exists():
                 return HttpResponse('Room already exists')
             else:
-                Room.objects.create(
-                    name=room_name,
-                    seat_number=room_capacity,
-                    projector=projector
-                    )
-                return HttpResponse('Room created')
+                Room.objects.create(name=room_name, seat_number=room_capacity, projector=projector)
+                return HttpResponse('Room added')
 
 
 def show_all_rooms(request):
@@ -38,3 +33,29 @@ def delete_room(request, room_id):
     room = Room.objects.get(pk=room_id)
     room.delete()
     return redirect('room_list')
+
+
+def modify_room(request, room_id):
+    if request.method == 'GET':
+        try:
+            room = Room.objects.get(pk=room_id)
+        except Room.DoesNotExist:
+            return HttpResponse(f'Room with id={room_id} does not exist')
+        else:
+            return render(request, 'service_app/modify_room.html', {'room': room})
+    elif request.method == 'POST':
+        try:
+            room_name = request.POST['name']
+            room_capacity = int(request.POST['seats_num'])
+            projector = request.POST.get('projector', False)
+        except MultiValueDictKeyError:
+            return HttpResponse('Invalid input data')
+        else:
+            if projector == 'True':
+                projector = True
+            room = Room.objects.get(pk=room_id)
+            room.name = room_name
+            room.seat_number = room_capacity
+            room.projector = projector
+            room.save()
+            return redirect('room_list')
