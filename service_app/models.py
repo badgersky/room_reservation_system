06223f5713cdf54406536fd63d65d85d534314a0
badgersky <szymonek.ptasznik@gmail.com>
2023-03-07 20:30:37 +0100

    created reservtion model, migrated model

--- service_app/models.py
+++ service_app/models.py
@@ -5,3 +5,13 @@ class Room(models.Model):
     name = models.CharField(max_length=255, unique=True)
     seat_number = models.SmallIntegerField()
     projector = models.BooleanField()
+
+
+class Reservation(models.Model):
+    date = models.DateField()
+    comment = models.TextField(null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('room', 'date')
+        
